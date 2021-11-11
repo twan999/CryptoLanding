46c6f91fc8c2ab0a34fdd8f06c94b550bad98906
Tuan <twan_dev@outlook.com>
2021-11-11 13:51:34 -0800

    debug with card on accordion

---
+++ src/components/Chart.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Chart from 'react-apexcharts'
+
+function CryptoChart() {
+  const chartOptions = {
+    options: {
+      chart: {
+        id: "basic-bar"
+      },
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+      }
+    },
+    series: [
+      {
+        name: "series-1",
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
+      }
+    ]
+  };
+
+  return (
+    <div className="mixed-chart">
+      <Chart
+        options={chartOptions.options}
+        series={chartOptions.series}
+        type="bar"
+        width="500"
+      />
+    </div>
+  )
+}
+
+export default CryptoChart
--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,12 +1,15 @@
-import Accordion from 'react-bootstrap/Accordion'
+import { Accordion, Card } from 'react-bootstrap'
+import CryptoChart from './Chart'
 import './Coin.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Coin = ({ name, image, symbol, price, volume, priceChange, marketcap, ath, ath_date, circulating_supply, max_supply }) => {
+
   return (
     <div className="coin-container">
       <Accordion className="coin-row">
         <Accordion.Item eventKey="0">
+          <Card>
           <Accordion.Header>
             <div className="coin">
               <img src={image} alt="crypto" />
@@ -18,17 +21,21 @@ const Coin = ({ name, image, symbol, price, volume, priceChange, marketcap, ath,
               <p className="coin-volume">${volume.toLocaleString()}</p>
               {priceChange < 0 ? (
                 <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
-              ) : (
-                <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
-              )}
+                ) : (
+                  <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
+                  )}
               <p className="coin-marketcap">Mkt Cap: ${marketcap.toLocaleString()}</p>
             </div>
           </Accordion.Header>
           <Accordion.Body className="coin-body">
-            <p>All Time High: ${ath.toLocaleString()} on {ath_date.slice(0,10)}</p>
-            <p>Circulating Supply: {circulating_supply}</p>
-            <p>Max Supply: { max_supply ? max_supply : 'Not Applicable'}</p>
+              <div className="coin-body-text">
+                <p>All Time High: ${ath.toLocaleString()} on {ath_date.slice(0,10)}</p>
+                <p>Circulating Supply: {circulating_supply}</p>
+                <p>Max Supply: { max_supply ? max_supply : 'Not Applicable'}</p>
+              </div>
+              <CryptoChart className="coin-chart" />
           </Accordion.Body>
+          </Card>
         </Accordion.Item>
       </Accordion>
     </div>
